04f85f89b779e4672642bf1ae4fc3e91ef10ab99
Unknown <vvykys@gmail.com>
2017-11-09 01:57:44 +0100

    updated bom script

--- hw/scripts/bom.py
+++ hw/scripts/bom.py
@@ -2,15 +2,34 @@
 
 url = '../bom/'
 boom_file = 'arcar.csv'
-shops_name = ('tme', 'farnell')
-
-shops = []
-for i in shops_name:
-    shops.append([])
 
 fr = open(url + boom_file, 'r')
 bom = fr.readlines()
 fr.close()
+
+def find_shops():
+    states = ('cz', 'en', 'us')
+    shops = set()
+    for s in bom:
+        if 'www.' in s:
+            s = s[s.find('www.') + 4:]
+        elif 'http://' in s:
+            s = s[s.find('http://') + 7:]
+        elif 'https://' in s:
+            s = s[s.find('https://') + 8:]
+        else:
+            continue
+        s = s.split('.')
+        s = s[1] if s[0] in states else s[0]
+        shops.add(s)
+    return tuple(shops)
+
+shops_name = find_shops()
+shops = []
+print('FIND SHOPS:')
+for i in shops_name:
+    print('\t\t-', i)
+    shops.append([])
 
 for s in range(len(bom)):
     for i in range(len(shops_name)):
