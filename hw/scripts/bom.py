7586ef64d4cc68da7e181824af7be7ebf58effea
Unknown <vvykys@gmail.com>
2017-11-09 01:13:50 +0100

    updated bom script and their outputs

--- hw/scripts/bom.py
+++ hw/scripts/bom.py
@@ -12,14 +12,10 @@ fr = open(url + boom_file, 'r')
 bom = fr.readlines()
 fr.close()
 
-
-for s in bom:
+for s in range(len(bom)):
     for i in range(len(shops_name)):
-        if shops_name[i] in s:
-            shops[i].append(s)
-        elif i == 1:
-            shops[i].append(s)
-
+        if not s or shops_name[i] in bom[s]:
+            shops[i].append(bom[s])
 
 for i in range(len(shops_name)):
     fw = open(url + shops_name[i] + '.csv', 'w')
